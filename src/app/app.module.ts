557b471ea12c7de7516681cfc3abf65d130c156d
brunocasabe22 <brunoabecas@gmail.com>
2023-01-15 19:28:43 -0300

    Test 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,15 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from "@angular/material/core";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {  HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,8 +31,17 @@ import { DialogComponent } from './dialog/dialog.component';
     MatIconModule,
     MatButtonModule,
     MatDialogModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    FormsModule,
+   HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/app/dialog/dialog.component.ts
+++ src/app/dialog/dialog.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
+import { ApiService } from '../services/api.service';
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss']
 })
-export class DialogComponent {
 
+
+export class DialogComponent implements OnInit {
+  reclamoForm !: FormGroup;
+  constructor(private formBuilder : FormBuilder, private api : ApiService){}
+ngOnInit(): void {
+    this.reclamoForm =  this.formBuilder.group({
+      reclamoNombre : ['',Validators.required],
+      area : ['',Validators.required],
+      status : ['',Validators.required],
+      dates  : ['',Validators.required],
+      comentario : ['',Validators.required]
+    })
+}
+addReclamo(){
+  console.log(this.reclamoForm.value);
+}
 }
