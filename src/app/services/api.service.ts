0e927925ea64bd5ea45e9222beb27fd076cb6c53
brunocasabe22 <brunoabecas@gmail.com>
2023-01-22 17:46:37 -0300

    22/01

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,15 +8,15 @@ export class ApiService {
 
   constructor(private http : HttpClient) { }
   postProduct(data :any){
-    return this.http.post<any>("http://172.16.0.15:3000/reclamoList/", data);
+    return this.http.post<any>("https://reclamos.onrender.com/reclamoList", data);
   }
   getProduct(){
-    return this.http.get<any>("http://172.16.0.15:3000/reclamoList/");
+    return this.http.get<any>("https://reclamos.onrender.com/reclamoList/");
   }
   putReclamo(data:any, id :number){
-    return this.http.put<any>("http://172.16.0.15:3000/reclamoList/"+id ,data)
+    return this.http.put<any>("https://reclamos.onrender.com/reclamoList"+id ,data)
   }
   deleteReclamo(id:number){
-    return this.http.delete<any>("http://172.16.0.15:3000/reclamoList/"+id)
+    return this.http.delete<any>("https://reclamos.onrender.com/reclamoList"+id)
   }
 }
